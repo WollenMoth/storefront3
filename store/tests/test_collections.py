dead53daa4c85c7509db1b34f7b49af8a42fcfc9
André Díaz <andre290800@gmail.com>
2021-12-21 16:17:47 -0600

    Running Tests.

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,7 +1,9 @@
 from rest_framework import status
 from rest_framework.test import APIClient
+import pytest
 
 
+@pytest.mark.django_db
 class TestCreateCollection:
     def test_if_user_is_anonymous_returns_401(self):
         client = APIClient()
