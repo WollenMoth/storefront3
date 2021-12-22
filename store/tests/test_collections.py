4c1e5ee3e037eef684361c5a8e387d532742dda1
André Díaz <andre290800@gmail.com>
2021-12-21 19:46:17 -0600

    Retrieve Collection Tests.

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,7 +1,7 @@
-from store.models import Collection
-from rest_framework import status
 import pytest
 from model_bakery import baker
+from rest_framework import status
+from store.models import Collection
 
 
 @pytest.fixture
@@ -9,6 +9,13 @@ def create_collection(api_client):
     def do_create_collection(collection):
         return api_client.post('/store/collections/', collection)
     return do_create_collection
+
+
+@pytest.fixture
+def retrieve_collection(api_client):
+    def do_retrieve_collection(collection_id):
+        return api_client.get(f'/store/collections/{collection_id}/')
+    return do_retrieve_collection
 
 
 @pytest.mark.django_db
@@ -44,10 +51,10 @@ class TestCreateCollection:
 
 @pytest.mark.django_db
 class TestRetrieveCollection:
-    def test_if_collection_exists_returns_200(self, api_client):
+    def test_if_collection_exists_returns_200(self, retrieve_collection):
         collection = baker.make(Collection)
 
-        response = api_client.get(f'/store/collections/{collection.id}/')
+        response = retrieve_collection(collection.id)
 
         assert response.status_code == status.HTTP_200_OK
         assert response.data == {
@@ -55,3 +62,8 @@ class TestRetrieveCollection:
             'title': collection.title,
             'products_count': 0
         }
+
+    def test_if_collection_does_not_exist_returns_404(self, retrieve_collection):
+        response = retrieve_collection(1)
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
