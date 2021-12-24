0416533a9bb89709a7c89c508a7258a12ca57142
André Díaz <andre290800@gmail.com>
2021-12-23 20:21:49 -0600

    Re-enabling Debug Toolbar.

--- storefront/settings.py
+++ storefront/settings.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
