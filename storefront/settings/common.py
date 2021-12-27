ce3b6a3d2ac7afdf556f8431374ec558dcfbccfc
André Díaz <andre290800@gmail.com>
2021-12-27 14:26:43 -0600

    Provisioning a Redis Instance.

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -161,23 +161,11 @@ ADMINS = [
     ('Andre Díaz', 'crhistian.diazb@alumno.buap.mx')
 ]
 
-CELERY_BROKER_URL = 'redis://localhost:6379/1'
 CELERY_BEAT_SCHEDULE = {
     'notify_customers': {
         'task': 'playground.tasks.notify_customers',
         'schedule': 5,
         'args': ['Hello World']
-    }
-}
-
-CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/2',
-        'TIMEOUT': 10 * 60,
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
     }
 }
 
--- storefront/settings/dev.py
+++ storefront/settings/dev.py
@@ -15,3 +15,16 @@ DATABASES = {
         'PASSWORD': 'Alitheia2908'
     }
 }
+
+CELERY_BROKER_URL = 'redis://localhost:6379/1'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/2',
+        'TIMEOUT': 10 * 60,
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
