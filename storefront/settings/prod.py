eed62be7a5651e2832c55c92ab45e10b63706e7b
André Díaz <andre290800@gmail.com>
2021-12-27 14:19:52 -0600

    Provisioning a MySQL Database.

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from .common import *
 
 DEBUG = False
@@ -6,3 +7,7 @@ DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ALLOWED_HOSTS = ['andrebuy-prod.herokuapp.com']
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
