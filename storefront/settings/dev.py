64b6db9892d8604b71458e9a69b0ca27d5f79e3e
André Díaz <andre290800@gmail.com>
2021-12-27 20:45:13 -0600

    Dockerizing the App.

--- storefront/settings/dev.py
+++ storefront/settings/dev.py
@@ -4,24 +4,24 @@ DEBUG = True
 
 SECRET_KEY = 'django-insecure-hs6j037urx6iav+7#10%-vu4l4f5@@-1_zo)oft4g7$vf2$jmp'
 
-# MIDDLEWARE += 'silk.middleware.SilkyMiddleware',
+MIDDLEWARE += 'silk.middleware.SilkyMiddleware',
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'storefront3',
-        'HOST': 'localhost',
+        'HOST': 'mysql',
         'USER': 'root',
         'PASSWORD': 'Alitheia2908'
     }
 }
 
-CELERY_BROKER_URL = 'redis://localhost:6379/1'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
 
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/2',
+        'LOCATION': 'redis://redis:6379/0',
         'TIMEOUT': 10 * 60,
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
@@ -29,7 +29,11 @@ CACHES = {
     }
 }
 
-EMAIL_HOST = 'localhost'
+EMAIL_HOST = 'smtp4dev'
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 2525
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': lambda request: True
+}
