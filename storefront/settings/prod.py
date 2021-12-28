46207cb3b52c7c6256a073b4deb92229f38f004c
André Díaz <andre290800@gmail.com>
2021-12-27 20:47:13 -0600

    Change allowed hosts.

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -6,7 +6,7 @@ DEBUG = False
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-ALLOWED_HOSTS = ['andrebuy-prod.herokuapp.com']
+ALLOWED_HOSTS = ['andrebuy.herokuapp.com']
 
 DATABASES = {
     'default': dj_database_url.config()
