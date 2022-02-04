b1698110af9c2bf51cda1025866f062cef88190a
André Díaz <andre290800@gmail.com>
2022-02-03 19:28:13 -0600

    Not Found.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { Fragment, useState, useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import NavBar from "./components/NavBar";
 import HomePage from "./components/HomePage";
+import NotFound from './components/NotFound';
 import Products from "./components/Products";
 import auth from "./services/authService";
 import "./App.css";
@@ -22,7 +23,9 @@ function App() {
       <main className="container">
         <Routes>
           <Route path="/products" element={<Products />} />
+          <Route path="/not-found" element={<NotFound />} />
           <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<Navigate to="/not-found" />} />
         </Routes>
       </main>
     </Fragment>
