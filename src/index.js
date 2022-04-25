401812960fc56350e69d7e0009495267c1bc3033
André Díaz <andre290800@gmail.com>
2022-04-24 23:03:04 -0500

    chore: update to client rendering API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import logger from "./services/logService";
@@ -9,11 +9,8 @@ import "react-toastify/dist/ReactToastify.css";
 
 logger.init();
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
 
 reportWebVitals();
