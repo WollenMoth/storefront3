dc800f704ce49312b85a62a4e26882307233fd98
André Díaz <andre290800@gmail.com>
2025-01-30 17:01:36 -0600

    refactor: ♻️ cart item get or create

--- store/serializers.py
+++ store/serializers.py
@@ -114,15 +114,14 @@ class AddCartItemSerializer(serializers.ModelSerializer):
         product_id = self.validated_data["product_id"]
         quantity = self.validated_data["quantity"]
 
-        try:
-            cart_item = CartItem.objects.get(cart_id=cart_id, product_id=product_id)
+        cart_item, exists = CartItem.objects.get_or_create(
+            cart_id=cart_id, product_id=product_id, defaults=self.validated_data
+        )
+
+        if exists:
             cart_item.quantity += quantity
             cart_item.save()
             self.instance = cart_item
-        except CartItem.DoesNotExist:
-            self.instance = CartItem.objects.create(
-                cart_id=cart_id, **self.validated_data
-            )
 
         return self.instance
 
