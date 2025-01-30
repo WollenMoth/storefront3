3a579c187727652ebcf377e8371c923812c9dfbf
André Díaz <andre290800@gmail.com>
2025-01-30 17:06:11 -0600

    remove: 🔥 separate frontend

--- src/services/userService.js
+++
@@ -1,7 +0,0 @@
-import http from "./httpService";
-
-const apiEndpoint = "/auth/users/";
-
-export function register(user) {
-  return http.post(apiEndpoint, user);
-}
--- storefront/urls.py
+++ storefront/urls.py
@@ -18,7 +18,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, re_path
-from django.views.generic import TemplateView
 
 admin.site.site_header = "Storefront Admin"
 admin.site.index_title = "Admin"
@@ -35,7 +34,3 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += [re_path(r"^silk/", include("silk.urls", namespace="silk"))]
-
-urlpatterns += [
-    re_path(r"^(?:.*)/?$", TemplateView.as_view(template_name="index.html"))
-]
